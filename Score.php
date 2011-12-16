5b7bf24e3dc8e75df2caa92b982f47591b24e85a
Mark A. Hershberger <mah@everybody.org>
2011-12-16 14:00:42 -0500

    style fixups, make sure directory is created, stop lilypod complaints about version

--- Score.php
+++ Score.php
@@ -1,25 +1,25 @@
 <?php
 /*
-    Score, a MediaWiki extension for rendering musical scores with LilyPond.
-    Copyright © 2011 Alexander Klauer
+	Score, a MediaWiki extension for rendering musical scores with LilyPond.
+	Copyright © 2011 Alexander Klauer
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    To contact the author:
-    <Graf.Zahl@gmx.net>
-    http://en.wikisource.org/wiki/User_talk:GrafZahl
-    https://github.com/TheCount/score
+	To contact the author:
+	<Graf.Zahl@gmx.net>
+	http://en.wikisource.org/wiki/User_talk:GrafZahl
+	https://github.com/TheCount/score
 
  */
 
@@ -91,5 +91,3 @@ function efScoreExtension( Parser &$parser ) {
 
 	return true;
 }
-
-?>
