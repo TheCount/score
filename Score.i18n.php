e455d659807352ff17b568f9ef4a6cfce35c0578
Raimond Spekking <raymond@users.mediawiki.org>
2011-12-19 20:15:36 +0000

    Localisation updates for core and extension messages from translatewiki.net

--- Score.i18n.php
+++ Score.i18n.php
@@ -58,3 +58,80 @@ $messages['qqq'] = array(
 	'score-notexecutable' => "Displayed if LilyPond binary can't be executed.",
 );
 
+/** German (Deutsch)
+ * @author Kghbln
+ */
+$messages['de'] = array(
+	'score-chdirerr' => 'Das Verzeichnis konnte nicht geändert werden',
+	'score-cleanerr' => 'Die alten Dateien konnten vor dem erneuten Rendern nicht bereinigt werden',
+	'score-compilererr' => 'Die Eingabedatei von LilyPond konnte nicht kompiliert werden:
+$1',
+	'score-desc' => 'Ergänzt das Tag <code><score></code>, welches das Rendern und Einbetten von Partituren mit LilyPond ermöglicht',
+	'score-getcwderr' => 'Das aktuelle Arbeitsverzeichnis konnte nicht aufgerufen werden',
+	'score-nooutput' => 'Das Bildverzeichnis für LilyPond konnte nicht erstellt werden',
+	'score-nofactory' => 'Das Arbeitsverzeichnis für LilyPond konnte nicht erstellt werden',
+	'score-noinput' => 'Die Eingabedatei für LilyPond konnte nicht erstellt werden',
+	'score-page' => 'Seite $1',
+	'score-renameerr' => 'Beim Verschieben der Partiturdateien in das Verzeichnis zum Hochladen ist ein Fehler aufgetreten',
+	'score-trimerr' => 'Das Bild konnte nicht zugeschnitten werden. In der Konfigurationsdatei muss <code>$wgScoreTrim = false;</code> festgelegt werden, sofern das Problem bestehen bleibt.',
+	'score-notexecutable' => 'LilyPond konnte nicht ausgeführt werden. Es muss sichergestellt sein, dass <code>$wgLilyPond</code> in der Konfigurationsdatei richtig eingestellt wurde.',
+);
+
+/** French (Français)
+ * @author Seb35
+ */
+$messages['fr'] = array(
+	'score-chdirerr' => 'Impossible de changer de répertoire',
+	'score-cleanerr' => 'Impossible d’effacer les anciens fichiers avant de regénérer',
+	'score-compilererr' => 'Impossible de compiler le fichier d’entrée LilyPond :
+$1',
+	'score-desc' => 'Ajoute une balise pour le rendu d’extraits musicaux avec LilyPond',
+	'score-getcwderr' => 'Impossible d’obtenir le répertoire de travail actuel',
+	'score-nooutput' => 'Erreur lors de la création du répertoire image de LilyPond',
+	'score-nofactory' => 'Erreur lors de la création du répertoire de la fabrique LilyPond',
+	'score-noinput' => 'Erreur lors de la création du fichier d’entrée LilyPond',
+	'score-page' => 'Page $1',
+	'score-renameerr' => 'Erreur lors du déplacement des fichiers de musique vers le répertoire de téléversement',
+	'score-trimerr' => 'L’image ne peut pas être redimensionnée. Configurez $wgScoreTrim=false si le problème persiste.',
+	'score-notexecutable' => 'Impossible d’exécuter LilyPond. Vérifiez que <code>$wgLilyPond</code> est correctement configuré.',
+);
+
+/** Macedonian (Македонски)
+ * @author Bjankuloski06
+ */
+$messages['mk'] = array(
+	'score-chdirerr' => 'Не можам да го сменам директориумот',
+	'score-cleanerr' => 'Не можам да ги исчистам старите податотеки пред да извршам повторен испис',
+	'score-compilererr' => 'Не можам да составам влезна податотека за LilyPond:
+$1',
+	'score-desc' => 'Додава ознака за испис на музички партитури со LilyPond',
+	'score-getcwderr' => 'Не можам да го добијам тековниот работен директориум',
+	'score-nooutput' => 'Не можев да создадам директориум за сликите на LilyPond',
+	'score-nofactory' => 'Не можев да создадам фабрички директориум за LilyPond',
+	'score-noinput' => 'Не можев да создадам влезна податотека за LilyPond',
+	'score-page' => 'Страница $1',
+	'score-renameerr' => 'Грешка при преместувањето на партитурните податотеки во директориумот за подигања',
+	'score-trimerr' => 'Не можев да ја скастрам сликата. Ако проблемот продолжи да се јавува, поставете $wgScoreTrim=false.',
+	'score-notexecutable' => 'Не можев да го пуштам LilyPond. Проверете дали <code>$wgLilyPond</code> е исправно наместено.',
+);
+
+/** Dutch (Nederlands)
+ * @author SPQRobin
+ * @author Siebrand
+ */
+$messages['nl'] = array(
+	'score-chdirerr' => 'Van map wisselen is niet mogelijk',
+	'score-cleanerr' => 'Het was niet mogelijk de oude bestanden op te ruimen voor het opnieuw aanmaken van de afbeeldingen',
+	'score-compilererr' => 'Het was niet mogelijk de LilyPondinvoer te compileren:
+$1',
+	'score-desc' => 'Voegt een label toe voor het weergeven van bladmuziek met LilyPond',
+	'score-getcwderr' => 'Het was niet mogelijk de ingestelde werkmap te gebruiken',
+	'score-nooutput' => 'Het was niet mogelijk de afbeeldingenmap voor LilyPond aan te maken',
+	'score-nofactory' => 'Het was niet mogelijk de factorymap voor LilyPond aan te maken',
+	'score-noinput' => 'Het was niet mogelijk het invoerbestand voor LilyPond aan te maken',
+	'score-page' => 'Pagina $1',
+	'score-renameerr' => 'Er is een fout opgetreden tijdens het verplaatsen van de bladmuziekbestanden naar de uploadmap',
+	'score-trimerr' => 'De afbeelding kon niet bijgesneden worden. Stel de volgende waarde in als dit probleem blijft bestaan: <code>$wgScoreTrim=false</code>.',
+	'score-notexecutable' => 'Het was niet mogelijk om LilyPond uit te voeren. Zorg dat <code>$wgLilyPond</code> correct is ingesteld.',
+);
+
